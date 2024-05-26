2d48a2b1aade52ad3a17c1a16478a365dda3ac13
Gabriel Lijenko <lijenkogabriel@hotmail.com>
2024-05-26 01:13:49 -0300

    Melhorias

--- app.py
+++ app.py
@@ -5,40 +5,36 @@ import sqlite3
 import calendar
 import locale
 import os
+from datetime import datetime, timedelta
+from itertools import groupby
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
 app = Flask(__name__)
-app.secret_key = b'\xf2B\x9c\x84\x91x\x0fq\xe7\xbd\x18\xe5\x1b\x13\x13P\x80so&\xc8\xd4\x1bi'  # Altere para uma chave secreta real
+app.secret_key = b'\xf2B\x9c\x84\x91x\x0fq\xe7\xbd\x18\xe5\x1b\x13\x13P\x80so&\xc8\xd4\x1bi'
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-# Configurar a localização para inglês (EUA)
 os.environ['LC_ALL'] = 'en_US.UTF-8'
 os.environ['LANG'] = 'en_US.UTF-8'
 
-# Função para criar a conexão com o banco de dados SQLite
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-# Criação das tabelas 'users' e 'agenda' no banco de dados SQLite
 def create_tables():
     conn = get_db_connection()
     try:
-        conn.execute('''CREATE TABLE IF NOT EXISTS users (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            username TEXT NOT NULL,
-                            email TEXT,
-                            password_hash TEXT NOT NULL)''')
         conn.execute('''CREATE TABLE IF NOT EXISTS agenda (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            date TEXT NOT NULL,
-                            description TEXT NOT NULL,
-                            completed INTEGER DEFAULT 0,  -- Adicionando a coluna "completed"
-                            user_id INTEGER,
-                            FOREIGN KEY (user_id) REFERENCES users (id))''')
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        date DATE NOT NULL,  -- Alteração para DATE
+                        description TEXT NOT NULL,
+                        completed INTEGER DEFAULT 0,
+                        user_id INTEGER,
+                        FOREIGN KEY (user_id) REFERENCES users (id))''')
         conn.commit()
     except Exception as e:
         print(f"Error creating tables: {e}")
@@ -66,7 +62,6 @@ def load_user(user_id):
         conn.close()
     return None
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -88,7 +83,6 @@ def register():
             conn.close()
     return render_template('register.html')
 
-
 @app.route('/change_password', methods=['GET', 'POST'])
 @login_required
 def change_password():
@@ -96,12 +90,9 @@ def change_password():
         old_password = request.form['old_password']
         new_password = request.form['new_password']
         
-        # Verificar se a senha antiga está correta
         if check_password_hash(current_user.password_hash, old_password):
-            # Gerar o hash da nova senha
             new_password_hash = generate_password_hash(new_password)
             
-            # Atualizar a senha no banco de dados
             conn = get_db_connection()
             try:
                 conn.execute('UPDATE users SET password_hash = ? WHERE id = ?', (new_password_hash, current_user.id))
@@ -148,85 +139,144 @@ def logout():
 def index():
     return render_template('index.html')
 
-# Criar as tabelas ao iniciar o aplicativo
 create_tables()
+
+@app.template_filter('nl2br')
+def nl2br(value):
+    return value.replace('\n', '<br>\n')
 
 @app.route('/agenda', methods=['GET', 'POST'])
 @login_required
 def agenda():
+    # Obter os parâmetros de filtro da URL
     selected_month = request.args.get('month')
     selected_year = request.args.get('year')
+
+    grouped_events = {}  # Inicializando grouped_events
 
     if request.method == 'POST':
         date = request.form['date']
         description = request.form['description']
 
-        # Formatar a data para yyyy-mm-dd
-        year, month, day = date.split('-')
-        formatted_date = f'{year}-{month}-{day}'
-
-        # Armazenar no SQLite
         conn = get_db_connection()
         try:
-            conn.execute('INSERT INTO agenda (date, description, user_id) VALUES (?, ?, ?)', (formatted_date, description, current_user.id))
+            conn.execute('INSERT INTO agenda (date, description, user_id) VALUES (?, ?, ?)', (date, description, current_user.id))
             conn.commit()
         except Exception as e:
             print(f"Error inserting agenda entry: {e}")
         finally:
             conn.close()
 
-    # Obter os dados do SQLite
+    # Buscar anos únicos da base de dados
+    years = []
+    try:
+        conn = get_db_connection()
+        cursor = conn.execute('SELECT DISTINCT strftime("%Y", date) as year FROM agenda WHERE user_id = ? ORDER BY year DESC', (current_user.id,))
+        years_data = cursor.fetchall()
+        years = [year[0] for year in years_data]  # Transforma a lista de tuplas em uma lista de anos únicos
+    except Exception as e:
+        print(f"Error fetching years: {e}")
+    finally:
+        conn.close()
+
+    # Buscar eventos agrupados por data
     conn = get_db_connection()
     try:
+        cursor = conn.cursor()
         if selected_month and selected_year:
-            cursor = conn.execute('SELECT * FROM agenda WHERE user_id = ? AND strftime("%m", date) = ? AND strftime("%Y", date) = ? ORDER BY date DESC', (current_user.id, selected_month, selected_year))
+            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? AND strftime("%m", date) = ? AND strftime("%Y", date) = ? ORDER BY date'
+            cursor.execute(query, (current_user.id, selected_month, selected_year))
         elif selected_month:
-            cursor = conn.execute('SELECT * FROM agenda WHERE user_id = ? AND strftime("%m", date) = ? ORDER BY date DESC', (current_user.id, selected_month))
+            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? AND strftime("%m", date) = ? ORDER BY date'
+            cursor.execute(query, (current_user.id, selected_month))
         elif selected_year:
-            cursor = conn.execute('SELECT * FROM agenda WHERE user_id = ? AND strftime("%Y", date) = ? ORDER BY date DESC', (current_user.id, selected_year))
+            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? AND strftime("%Y", date) = ? ORDER BY date'
+            cursor.execute(query, (current_user.id, selected_year))
         else:
-            cursor = conn.execute('SELECT * FROM agenda WHERE user_id = ? ORDER BY date DESC', (current_user.id,))
+            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? ORDER BY date'
+            cursor.execute(query, (current_user.id,))
         agenda_data = cursor.fetchall()
-
-        # Converter as datas para o formato dd-mm-yyyy para exibição
-        agenda_data = [dict(row) for row in agenda_data]
         for entry in agenda_data:
-            year, month, day = entry['date'].split('-')
-            entry['date'] = f'{day}-{month}-{year}'
+            entry_date = entry[1]
+            if not isinstance(entry_date, str):
+                entry_date = entry_date.strftime('%d-%m-%Y')
+            if entry_date not in grouped_events:
+                grouped_events[entry_date] = []
+            grouped_events[entry_date].append({'id': entry[0], 'date': entry_date, 'description': entry[2]})
     except Exception as e:
         print(f"Error fetching agenda data: {e}")
         agenda_data = []
     finally:
         conn.close()
 
-    # Obter lista de meses e anos disponíveis
     months = []
-    years = []
     try:
-        conn = get_db_connection()  # Nova conexão
-        cursor = conn.execute('SELECT DISTINCT strftime("%m", date) as month, strftime("%Y", date) as year FROM agenda WHERE user_id = ? ORDER BY year DESC, month DESC', (current_user.id,))
+        conn = get_db_connection()
+        cursor = conn.execute('SELECT DISTINCT strftime("%m", date) as month FROM agenda WHERE user_id = ? ORDER BY month', (current_user.id,))
         months_data = cursor.fetchall()
-        seen_months = set()
         for month_data in months_data:
-            if month_data['month'] not in seen_months:
-                month_name = calendar.month_name[int(month_data['month'])].capitalize()
-                months.append({'month': month_data['month'], 'month_name': month_name})
-                seen_months.add(month_data['month'])
-            if month_data['year'] not in years:
-                years.append(month_data['year'])
+            month_name = calendar.month_name[int(month_data[0])].capitalize()
+            months.append({'month': month_data[0], 'month_name': month_name})
     except Exception as e:
         print(f"Error fetching months: {e}")
     finally:
         conn.close()
 
-    return render_template('agenda.html', agenda_data=agenda_data, months=months, years=years, selected_month=selected_month, selected_year=selected_year)
+    return render_template('agenda.html', agenda_data=agenda_data, months=months, years=years, selected_month=selected_month, selected_year=selected_year, grouped_events=grouped_events)
 
+
+
+def group_by_weeks(agenda_data):
+    if not agenda_data:
+        return []
+
+    agenda_data.sort(key=lambda x: datetime.strptime(x['date'], '%d-%m-%Y'))
+    grouped_data = []
+    current_week = []
+    start_of_week = datetime.strptime(agenda_data[0]['date'], '%d-%m-%Y')
+    end_of_week = start_of_week + timedelta(days=6)
+
+    for entry in agenda_data:
+        entry_date = entry.get('date')
+        if entry_date:
+            if isinstance(entry_date, str):  # Garante que a data seja uma string para comparação
+                entry_date = datetime.strptime(entry_date, '%d-%m-%Y')
+            entry['date'] = entry_date.strftime('%Y-%m-%d')
+            if entry_date > end_of_week:
+                grouped_data.append({'week_start': start_of_week, 'week_end': end_of_week, 'entries': current_week})
+                current_week = []
+                start_of_week = entry_date - timedelta(days=entry_date.weekday())
+                end_of_week = start_of_week + timedelta(days=6)
+            current_week.append(entry)
+
+    if current_week:
+        grouped_data.append({'week_start': start_of_week, 'week_end': end_of_week, 'entries': current_week})
+
+    return grouped_data
+
+@app.route('/edit_event/<int:event_id>', methods=['GET', 'POST'])
+@login_required
+def edit_event(event_id):
+    if request.method == 'POST':
+        new_description = request.form['new_description']
+        
+        conn = get_db_connection()
+        try:
+            conn.execute('UPDATE agenda SET description = ? WHERE id = ?', (new_description, event_id))
+            conn.commit()
+            flash('Evento editado com sucesso!')
+            return redirect(url_for('agenda'))
+        except Exception as e:
+            print(f"Error editing event: {e}")
+            flash('Erro ao editar o evento. Por favor, tente novamente.')
+        finally:
+            conn.close()
+    return redirect(url_for('agenda'))  # Redirecionar para a página de agenda em caso de método GET
 
 @app.route('/admin', methods=['GET'])
 @login_required
 def admin():
-    # Verificar se o usuário atual é um administrador
-    if current_user.username != 'vigoradmin':
+    if current_user.username != 'teste':
         flash('Acesso negado.')
         return redirect(url_for('index'))
     
@@ -246,28 +296,9 @@ def admin():
 
     return render_template('admin.html', users=users, agenda=agenda)
 
-@app.route('/edit/<int:entry_id>', methods=['POST'])
-@login_required
-def edit_entry(entry_id):
-    new_description = request.form['new_description']
-
-    # Atualizar a descrição no SQLite
-    conn = get_db_connection()
-    try:
-        conn.execute('UPDATE agenda SET description = ? WHERE id = ? AND user_id = ?', (new_description, entry_id, current_user.id))
-        conn.commit()
-    except Exception as e:
-        print(f"Error updating agenda entry: {e}")
-    finally:
-        conn.close()
-
-    flash('Evento atualizado com sucesso!')
-    return redirect(url_for('agenda'))
-
 @app.route('/delete/<int:entry_id>')
 @login_required
 def delete_entry(entry_id):
-    # Excluir a entrada do SQLite
     conn = get_db_connection()
     try:
         conn.execute('DELETE FROM agenda WHERE id = ? AND user_id = ?', (entry_id, current_user.id))
@@ -294,7 +325,6 @@ def mark_complete(entry_id):
     finally:
         conn.close()
 
-    return redirect(url_for('agenda'))
 
 if __name__ == '__main__':
     app.run(debug=True)
