cc533c307c1d2ffb33772d9d30f4bf8add3730fd
Gabriel Lijenko <lijenkogabriel@hotmail.com>
2024-05-28 21:57:27 -0300

    Configuração do PostgreSQL

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timedelta
 import calendar
 import locale
-import os
-from datetime import datetime, timedelta
-from itertools import groupby
 
 try:
     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
@@ -16,54 +15,33 @@ except locale.Error:
 app = Flask(__name__)
 app.secret_key = b'\xf2B\x9c\x84\x91x\x0fq\xe7\xbd\x18\xe5\x1b\x13\x13P\x80so&\xc8\xd4\x1bi'
 
+# Configurações do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://ufs9evrb41pi2m:p6a1b5faa8d58943e6d3fd9f03b8aecfa58cf07c6ffeb8b1dc316f56ef8e8e230@ceqbglof0h8enj.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2vu17d0p14l4e').replace('postgres://', 'postgresql://')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-os.environ['LC_ALL'] = 'en_US.UTF-8'
-os.environ['LANG'] = 'en_US.UTF-8'
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password_hash = db.Column(db.String(150), nullable=False)
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+class Agenda(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-def create_tables():
-    conn = get_db_connection()
-    try:
-        conn.execute('''CREATE TABLE IF NOT EXISTS agenda (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        date DATE NOT NULL,  -- Alteração para DATE
-                        description TEXT NOT NULL,
-                        completed INTEGER DEFAULT 0,
-                        user_id INTEGER,
-                        FOREIGN KEY (user_id) REFERENCES users (id))''')
-        conn.commit()
-    except Exception as e:
-        print(f"Error creating tables: {e}")
-    finally:
-        conn.close()
-
-class User(UserMixin):
-    def __init__(self, id, username, email, password_hash):
-        self.id = id
-        self.username = username
-        self.email = email
-        self.password_hash = password_hash
 
 @login_manager.user_loader
 def load_user(user_id):
-    conn = get_db_connection()
-    try:
-        cursor = conn.execute('SELECT * FROM users WHERE id = ?', (user_id,))
-        user = cursor.fetchone()
-        if user:
-            return User(id=user['id'], username=user['username'], email=user['email'], password_hash=user['password_hash'])
-    except Exception as e:
-        print(f"Error loading user: {e}")
-    finally:
-        conn.close()
-    return None
+    return User.query.get(int(user_id))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -72,18 +50,13 @@ def register():
         email = request.form['email']
         password = request.form['password']
         password_hash = generate_password_hash(password)
-        
-        conn = get_db_connection()
-        try:
-            conn.execute('INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)', (username, email, password_hash))
-            conn.commit()
-            flash('Registered successfully. Please log in.')
-            return redirect(url_for('login'))
-        except Exception as e:
-            print(f"Error registering user: {e}")
-            flash('An error occurred during registration. Please try again.')
-        finally:
-            conn.close()
+
+        new_user = User(username=username, email=email, password_hash=password_hash)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registered successfully. Please log in.')
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 @app.route('/change_password', methods=['GET', 'POST'])
@@ -92,20 +65,12 @@ def change_password():
     if request.method == 'POST':
         old_password = request.form['old_password']
         new_password = request.form['new_password']
-        
+
         if check_password_hash(current_user.password_hash, old_password):
             new_password_hash = generate_password_hash(new_password)
-            
-            conn = get_db_connection()
-            try:
-                conn.execute('UPDATE users SET password_hash = ? WHERE id = ?', (new_password_hash, current_user.id))
-                conn.commit()
-                flash('Senha alterada com sucesso!')
-            except Exception as e:
-                print(f"Error changing password: {e}")
-                flash('Erro ao alterar a senha. Por favor, tente novamente.')
-            finally:
-                conn.close()
+            current_user.password_hash = new_password_hash
+            db.session.commit()
+            flash('Senha alterada com sucesso!')
         else:
             flash('Senha antiga incorreta.')
     
@@ -117,19 +82,12 @@ def login():
         username = request.form['username']
         password = request.form['password']
         
-        conn = get_db_connection()
-        try:
-            cursor = conn.execute('SELECT * FROM users WHERE username = ?', (username,))
-            user = cursor.fetchone()
-            if user and check_password_hash(user['password_hash'], password):
-                login_user(User(id=user['id'], username=user['username'], email=user['email'], password_hash=user['password_hash']))
-                return redirect(url_for('agenda'))
-            else:
-                flash('Invalid username or password')
-        except Exception as e:
-            print(f"Error logging in user: {e}")
-        finally:
-            conn.close()
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            return redirect(url_for('agenda'))
+        else:
+            flash('Invalid username or password')
     return render_template('login.html')
 
 @app.route('/logout')
@@ -142,139 +100,53 @@ def logout():
 def index():
     return render_template('index.html')
 
-create_tables()
-
-@app.template_filter('nl2br')
-def nl2br(value):
-    return value.replace('\n', '<br>\n')
-
 @app.route('/agenda', methods=['GET', 'POST'])
 @login_required
 def agenda():
-    # Obter os parâmetros de filtro da URL
     selected_month = request.args.get('month')
     selected_year = request.args.get('year')
-
-    grouped_events = {}  # Inicializando grouped_events
+    grouped_events = {}
 
     if request.method == 'POST':
         date = request.form['date']
         description = request.form['description']
+        new_event = Agenda(date=date, description=description, user_id=current_user.id)
+        db.session.add(new_event)
+        db.session.commit()
 
-        conn = get_db_connection()
-        try:
-            conn.execute('INSERT INTO agenda (date, description, user_id) VALUES (?, ?, ?)', (date, description, current_user.id))
-            conn.commit()
-        except Exception as e:
-            print(f"Error inserting agenda entry: {e}")
-        finally:
-            conn.close()
+    years = [year[0] for year in db.session.query(db.func.strftime("%Y", Agenda.date)).distinct().filter_by(user_id=current_user.id).all()]
+    months = [{'month': month[0], 'month_name': calendar.month_name[int(month[0])].capitalize()} for month in db.session.query(db.func.strftime("%m", Agenda.date)).distinct().filter_by(user_id=current_user.id).all()]
 
-    # Buscar anos únicos da base de dados
-    years = []
-    try:
-        conn = get_db_connection()
-        cursor = conn.execute('SELECT DISTINCT strftime("%Y", date) as year FROM agenda WHERE user_id = ? ORDER BY year DESC', (current_user.id,))
-        years_data = cursor.fetchall()
-        years = [year[0] for year in years_data]  # Transforma a lista de tuplas em uma lista de anos únicos
-    except Exception as e:
-        print(f"Error fetching years: {e}")
-    finally:
-        conn.close()
-
-    # Buscar eventos agrupados por data
-    conn = get_db_connection()
-    try:
-        cursor = conn.cursor()
-        if selected_month and selected_year:
-            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? AND strftime("%m", date) = ? AND strftime("%Y", date) = ? ORDER BY date'
-            cursor.execute(query, (current_user.id, selected_month, selected_year))
-        elif selected_month:
-            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? AND strftime("%m", date) = ? ORDER BY date'
-            cursor.execute(query, (current_user.id, selected_month))
-        elif selected_year:
-            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? AND strftime("%Y", date) = ? ORDER BY date'
-            cursor.execute(query, (current_user.id, selected_year))
-        else:
-            query = 'SELECT id, date, description FROM agenda WHERE user_id = ? ORDER BY date'
-            cursor.execute(query, (current_user.id,))
-        agenda_data = cursor.fetchall()
-        for entry in agenda_data:
-            entry_date = entry[1]
-            if not isinstance(entry_date, str):
-                entry_date = entry_date.strftime('%d-%m-%Y')
-            if entry_date not in grouped_events:
-                grouped_events[entry_date] = []
-            grouped_events[entry_date].append({'id': entry[0], 'date': entry_date, 'description': entry[2]})
-    except Exception as e:
-        print(f"Error fetching agenda data: {e}")
-        agenda_data = []
-    finally:
-        conn.close()
-
-    months = []
-    try:
-        conn = get_db_connection()
-        cursor = conn.execute('SELECT DISTINCT strftime("%m", date) as month FROM agenda WHERE user_id = ? ORDER BY month', (current_user.id,))
-        months_data = cursor.fetchall()
-        for month_data in months_data:
-            month_name = calendar.month_name[int(month_data[0])].capitalize()
-            months.append({'month': month_data[0], 'month_name': month_name})
-    except Exception as e:
-        print(f"Error fetching months: {e}")
-    finally:
-        conn.close()
-
-    return render_template('agenda.html', agenda_data=agenda_data, months=months, years=years, selected_month=selected_month, selected_year=selected_year, grouped_events=grouped_events)
-
-
-
-def group_by_weeks(agenda_data):
-    if not agenda_data:
-        return []
-
-    agenda_data.sort(key=lambda x: datetime.strptime(x['date'], '%d-%m-%Y'))
-    grouped_data = []
-    current_week = []
-    start_of_week = datetime.strptime(agenda_data[0]['date'], '%d-%m-%Y')
-    end_of_week = start_of_week + timedelta(days=6)
+    query = Agenda.query.filter_by(user_id=current_user.id)
+    if selected_month and selected_year:
+        query = query.filter(db.func.strftime("%m", Agenda.date) == selected_month, db.func.strftime("%Y", Agenda.date) == selected_year)
+    elif selected_month:
+        query = query.filter(db.func.strftime("%m", Agenda.date) == selected_month)
+    elif selected_year:
+        query = query.filter(db.func.strftime("%Y", Agenda.date) == selected_year)
+    agenda_data = query.order_by(Agenda.date).all()
 
     for entry in agenda_data:
-        entry_date = entry.get('date')
-        if entry_date:
-            if isinstance(entry_date, str):  # Garante que a data seja uma string para comparação
-                entry_date = datetime.strptime(entry_date, '%d-%m-%Y')
-            entry['date'] = entry_date.strftime('%Y-%m-%d')
-            if entry_date > end_of_week:
-                grouped_data.append({'week_start': start_of_week, 'week_end': end_of_week, 'entries': current_week})
-                current_week = []
-                start_of_week = entry_date - timedelta(days=entry_date.weekday())
-                end_of_week = start_of_week + timedelta(days=6)
-            current_week.append(entry)
+        entry_date = entry.date.strftime('%d-%m-%Y')
+        if entry_date not in grouped_events:
+            grouped_events[entry_date] = []
+        grouped_events[entry_date].append({'id': entry.id, 'date': entry_date, 'description': entry.description})
 
-    if current_week:
-        grouped_data.append({'week_start': start_of_week, 'week_end': end_of_week, 'entries': current_week})
-
-    return grouped_data
+    return render_template('agenda.html', agenda_data=agenda_data, months=months, years=years, selected_month=selected_month, selected_year=selected_year, grouped_events=grouped_events)
 
 @app.route('/edit_event/<int:event_id>', methods=['GET', 'POST'])
 @login_required
 def edit_event(event_id):
     if request.method == 'POST':
         new_description = request.form['new_description']
-        
-        conn = get_db_connection()
-        try:
-            conn.execute('UPDATE agenda SET description = ? WHERE id = ?', (new_description, event_id))
-            conn.commit()
+        event = Agenda.query.get(event_id)
+        if event and event.user_id == current_user.id:
+            event.description = new_description
+            db.session.commit()
             flash('Evento editado com sucesso!')
-            return redirect(url_for('agenda'))
-        except Exception as e:
-            print(f"Error editing event: {e}")
+        else:
             flash('Erro ao editar o evento. Por favor, tente novamente.')
-        finally:
-            conn.close()
-    return redirect(url_for('agenda'))  # Redirecionar para a página de agenda em caso de método GET
+    return redirect(url_for('agenda'))
 
 @app.route('/admin', methods=['GET'])
 @login_required
@@ -282,52 +154,35 @@ def admin():
     if current_user.username != 'teste':
         flash('Acesso negado.')
         return redirect(url_for('index'))
-    
-    conn = get_db_connection()
-    try:
-        users_cursor = conn.execute('SELECT id, username FROM users')
-        users = users_cursor.fetchall()
 
-        agenda_cursor = conn.execute('SELECT * FROM agenda')
-        agenda = agenda_cursor.fetchall()
-    except Exception as e:
-        print(f"Error fetching admin data: {e}")
-        users = []
-        agenda = []
-    finally:
-        conn.close()
-
+    users = User.query.all()
+    agenda = Agenda.query.all()
     return render_template('admin.html', users=users, agenda=agenda)
 
 @app.route('/delete/<int:entry_id>')
 @login_required
 def delete_entry(entry_id):
-    conn = get_db_connection()
-    try:
-        conn.execute('DELETE FROM agenda WHERE id = ? AND user_id = ?', (entry_id, current_user.id))
-        conn.commit()
-    except Exception as e:
-        print(f"Error deleting agenda entry: {e}")
-    finally:
-        conn.close()
-
-    flash('Evento excluído com sucesso!')
+    event = Agenda.query.get(entry_id)
+    if event and event.user_id == current_user.id:
+        db.session.delete(event)
+        db.session.commit()
+        flash('Evento excluído com sucesso!')
+    else:
+        flash('Erro ao excluir o evento.')
     return redirect(url_for('agenda'))
 
 @app.route('/mark_complete/<int:entry_id>', methods=['GET'])
 @login_required
 def mark_complete(entry_id):
-    conn = get_db_connection()
-    try:
-        conn.execute('UPDATE agenda SET completed = 1 WHERE id = ? AND user_id = ?', (entry_id, current_user.id))
-        conn.commit()
+    event = Agenda.query.get(entry_id)
+    if event and event.user_id == current_user.id:
+        event.completed = True
+        db.session.commit()
         return jsonify({'message': 'Tarefa marcada como concluída.'})
-    except Exception as e:
-        print(f"Error marking task as complete: {e}")
+    else:
         return jsonify({'message': 'Erro ao marcar a tarefa como concluída.'}), 500
-    finally:
-        conn.close()
-
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
